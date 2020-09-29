4bc60de491e495858c978056323ea43eef8d31a9
bigzhezhe-zhang <66518107+bigzhezhe-zhang@users.noreply.github.com>
2020-09-29 15:45:20 +0800

    测试1

---
+++ Test1/src/com/itheima/git/Hello.java
@@ -0,0 +1,13 @@
+package com.itheima.git;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("test1");
+        ls();
+    }
+    public static void ls(){
+        for (int i=10;i>0;i++){
+            System.out.println(i);
+        }
+    }
+}
