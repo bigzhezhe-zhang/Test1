15c7ed94e3b27c68e0ff17962c24af8f724dffd3
bigzhezhe-zhang <66518107+bigzhezhe-zhang@users.noreply.github.com>
2020-09-29 15:54:10 +0800

    测试2

--- Test1/src/com/itheima/git/Hello.java
+++ Test1/src/com/itheima/git/Hello.java
@@ -3,10 +3,16 @@ package com.itheima.git;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("test1");
+        System.out.println("test1");
+        System.out.println("test1");
+        System.out.println("test1");
+        System.out.println("test1");
+        System.out.println("test1");
+
         ls();
     }
     public static void ls(){
-        for (int i=10;i>0;i++){
+        for (int i=10;i>0;i--){
             System.out.println(i);
         }
     }
